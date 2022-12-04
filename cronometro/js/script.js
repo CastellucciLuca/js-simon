3ce3e58ed6c6a83ec0cc899e78ab69823f965a79
CastellucciLuca <castellucciluca98@gmail.com>
2022-12-04 10:44:39 +0100

    cronometro-cleancode

--- cronometro/js/script.js
+++ cronometro/js/script.js
@@ -1,6 +1,57 @@
-const tempo = document.getElementById('tempo');
-const start = document.getElementById('start');
-const pause = document.getElementById('pause');
-const reset = document.getElementById('reset');
+console.log('Hello World!');
+
+let centsElement = document.getElementById('cents');
+let decSecElement = document.getElementById('decSec');
+let secondsElement = document.getElementById('seconds');
+let tenSecsElement = document.getElementById('tenSecs');
+let minutesElement = document.getElementById('minutes');
+let tenMinutesElement = document.getElementById('tenMinutes');
+
+let avvia = document.getElementById('avvia');
+let pausa = document.getElementById('pausa');
+let azzera = document.getElementById('azzera');
+
+let cents = 0;
+let decSec = 0;
+let seconds = 0;
+let tenSecs = 0;
+let minutes = 0;
+let tenMinutes = 0;
 
 
+
+function getNumberScrollingTo10(value, element, time) {
+    setInterval(() => {
+        if (value < 9) {
+            value++;
+            element.innerHTML = value
+        } else {
+            value = 0
+            element.innerHTML = value
+        }
+    }, time);
+}
+
+function getNumberScrollingTo6(value, element, time) {
+    setInterval(() => {
+        if (value < 6) {
+            value++;
+            element.innerHTML = value
+        } else {
+            value = 0
+            element.innerHTML = value
+        }
+    }, time);
+}
+
+
+
+
+avvia.addEventListener("click", function () {
+    getNumberScrollingTo10(cents, centsElement, 10);
+    getNumberScrollingTo10(decSec, decSecElement, 100);
+    getNumberScrollingTo10(seconds, secondsElement, 1000);
+    getNumberScrollingTo6(tenSecs, tenSecsElement, 10000);
+    getNumberScrollingTo10(minutes, minutesElement, 60000);
+    getNumberScrollingTo6(tenSecs, tenMinutesElement, 600000);
+})
